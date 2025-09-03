2f7371913d6d83c1321e912d4395a3899a6cb3e1
Rohit Nair <rohitcse081@gmail.com>
2025-09-04 03:25:32 +0530

    Setup user-schema with prisma

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,26 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
-export type User = {
-  user_id: number;
-  username: string;
-  password: string;
-}
+import { Prisma, User } from 'generated/prisma';
+import { PrismaService } from 'src/data/prisma.service';
 
 @Injectable()
 export class UsersService {
-  private readonly users: Array<User> = [
-    {
-      user_id: 1,
-      username: 'john',
-      password: 'password'
-    }, {
-      user_id: 2,
-      username: 'doe',
-      password: 'passwords'
-    }
-  ];
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+  constructor(private prisma: PrismaService) {}
+
+  async user(filter: Prisma.UserWhereUniqueInput): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: filter
+    })
+  }
+
+  async findOne(username: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        username: username
+      }
+    });
   }
 }
