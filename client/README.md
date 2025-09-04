4db4b913ad8e0a8571a6c008c95e9ce38d0a3b08
Rohit Nair <rohitcse081@gmail.com>
2025-09-04 19:59:20 +0530

    [Client] Added header and home components

--- client/README.md
+++ client/README.md
@@ -57,3 +57,4 @@ Angular CLI does not come with an end-to-end testing framework by default. You c
 ## Additional Resources
 
 For more information on using the Angular CLI, including detailed command references, visit the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+- [UI Template](https://www.figma.com/design/ekYAwq5PYGX0nui8BzopEx/Maglo---Financial-Management-Web-UI-Kit--Community-?node-id=0-1&p=f&t=gExCupv4h7zPMoZM-0)
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Header],
   templateUrl: './app.html',
-  styleUrl: './app.sass'
+  styleUrl: './app.sass',  
 })
 export class App {
-  protected readonly title = signal('client');
+  protected readonly title = signal('MoneyDiary');
 }
